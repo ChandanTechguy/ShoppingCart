01c58a3c11f914c0e806782af9122d9dbc0d6187
test <test@TM-ITDesktop.TECHMOJO.LOCAL>
2022-07-25 18:42:18 +0530

    initial

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,67 +1,149 @@
-import React from 'react'
-import './Login.css'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
 
+export const Loginform = () => {
+    let navigate = useNavigate();
+    const [username, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const mediumRegex = new RegExp(
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+        );
+        if (!password.match(mediumRegex)) {
+            alert("Please enter a strong password");
+        } else {
+            navigate("/products");
+        }
+    };
 
- export const Loginform = () => {
-    const navigate = useNavigate()
     return (
-        <div className='Chandan'>
-            
-            <form className='form-container '>
-                <ul className="nav nav-pills nav-justified mb-3 " id="ex1" role="tablist">
+        <div className="Chandan">
+            {/* <form className="form-container " onSubmit={handleSubmit}>
+                <ul
+                    className="nav nav-pills nav-justified mb-3 "
+                    id="ex1"
+                    role="tablist"
+                >
                     <li className="nav-item" role="presentation">
-                        <a class="nav-link active" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab"
-                            aria-controls="pills-login" aria-selected="true">Login</a>
+                        <a
+                            class="nav-link active"
+                            id="tab-login"
+                            data-mdb-toggle="pill"
+                            href="#pills-login"
+                            role="tab"
+                            aria-controls="pills-login"
+                            aria-selected="true"
+                        >
+                            Login
+                        </a>
                     </li>
-
                 </ul>
                 <div className="tab-content">
-                    <div className="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
-                        <form>
-
-                            <div className="form-outline mb-4">
-                                <label className="form-label" htmlFor="loginName" id="Das" >Email or Username</label>
-                                <input required minLength={8}  type="email" id="loginName" className="form-control" />
+                    <div
+                        className="tab-pane fade show active"
+                        id="pills-login"
+                        role="tabpanel"
+                        aria-labelledby="tab-login"
+                    > */}
+                        <form className="form-container border " onSubmit={handleSubmit}>
+                        <ul
+                    className="nav nav-pills nav-justified mb-3 "
+                    id="ex1"
+                    role="tablist"
+                >
+                    <li className="nav-item" role="presentation">
+                        <a
+                            class="nav-link active"
+                            id="tab-login"
+                            data-mdb-toggle="pill"
+                            href="#pills-login"
+                            role="tab"
+                            aria-controls="pills-login"
+                            aria-selected="true"
+                        >
+                           <strong> Login</strong>
+                        </a>
+                    </li>
+                </ul>
+                            <div className="was-validated container container-sm ">
+                                <label className="form-label" htmlFor="loginName" id="Das"><strong> Email or Username</strong>
+                                   
+                                </label>
+                                <input
+                                    required
+                                    minLength={8}
+                                    type="email"
+                                    id="loginName"
+                                    className="form-control"
+                                    value={username}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="Email"
+                                    
+                                />
 
                                 {/* {err.email && (<div>
                                         <p class ="danger">{err.email}</p>
                             </div>)
                                 } */}
                             </div>
-                            
-                            
-                            <div className="form-outline mb-4">
-                                <label className="form-label" htmlFor="loginPassword" id="Text">Password</label>
-                                <input type="password" id="loginPassword" className="form-control" />
+                            <br></br>
 
+                            <div className="was-validated container container-sm ">
+                                <label className="form-label" htmlFor="loginPassword" id="Text"><strong>Password</strong>
+                                  
+                                </label>
+                                <input
+                                    type="password"
+                                    id="loginPassword"
+                                    className="form-control"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    placeholder="Password"
+                                    required
+                                />
                             </div>
-
+                            <br></br>
 
                             <div className="row mb-4">
                                 <div className="col-md-6 d-flex justify-content-center">
-
                                     <div className="form-check mb-3 mb-md-0">
-                                        <input className="form-check-input" type="checkbox" value="" id="loginCheck" checked />
-                                        <label className="form-check-label" id="cha" htmlFor="loginCheck"> Remember me </label>
+                                        <input
+                                            className="form-check-input"
+                                            type="checkbox"
+                                            value=""
+                                            id="loginCheck"
+                                            checked
+                                        />
+                                        <label
+                                            className="form-check-label"
+                                            id="cha"
+                                            htmlFor="loginCheck"
+                                        >
+                                            {" "}
+                                            Remember me{" "}
+                                        </label>
                                     </div>
                                 </div>
 
-                                <div className="col-md-6 d-flex justify-content-center" >
-                                    <a href="#!" id="pass">Forgot password ?</a>
+                                <div className="col-md-6 d-flex justify-content-center">
+                                    <a href="#!" id="pass"><strong>Forgot password ?</strong>
+                                        
+                                    </a>
                                 </div>
                             </div>
 
-
-                            <button onClick={() => { navigate('/products') }} type="submit" className="btn btn-primary btn-block mb-4 container" >Sign in</button>
+                            <button
+                                className="btn btn-primary btn-block mb-4 container"
+                            >
+                                <strong>Sign in</strong>
+                            </button>
                         </form>
                     </div>
-                </div>
-
-            </form>
-        </div>
-
+            //     </div>
+            // </form>
+        // </div>
     );
-}
-
+};
