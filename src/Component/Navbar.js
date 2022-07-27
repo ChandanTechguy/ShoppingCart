a08cd286b74664a13bb23a2c12203af4057a5c1d
ChandanTechguy <109069555+ChandanTechguy@users.noreply.github.com>
2022-07-27 16:48:39 +0530

    Update Navbar.js

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -9,7 +9,10 @@ export default function Navbar() {
        <div style={show}>
          <h3 ><strong>Let's Shopping !! </strong></h3>
          <input type="text" style= {{ height:"30px",border:'bold', borderRadius:"20px"}} placeholder="Search" />
-        
+         <div className="add-to-cart">
+         <span></span>
+         <button style={{display:"corner"}}><img src="https://cdn.iconscout.com/icon/free/png-512/shopping-cart-442-1151214.png" alt="cart" onClick={()=>{navigate(`/cart`)}} style={{width:"40px", display:"corner"}}/></button>
+      </div>
          <div>
         </div>
        </div>
